e79877732a0ae87c946330afa82dd636e8e0f28f
drfaust92 <ilia.lazebnik@gmail.com>
2021-10-01 19:49:42 +0300

    ci

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/chesshacker/terraform-provider-confluence
 
-go 1.13
+go 1.16
 
 require (
 	github.com/aws/aws-sdk-go v1.28.5 // indirect
--- scripts/get-golangci.sh
+++ scripts/get-golangci.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-GOLANGCI_VERSION=v1.22.2
+GOLANGCI_VERSION=v1.42.1
 
 mkdir -p bin
 curl -sSfL "https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh" \
